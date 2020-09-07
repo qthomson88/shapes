077eae37810f6e8508d6a9b35efbbe65c2d8234c
qthomson88@gmail.com <qthomson88@gmail.com>
2020-09-07 16:23:19 -0600

    added a few test cases

---
+++ shapesUnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("shapesUnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("shapesUnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("be85d010-35ab-4430-962d-6102df6568d7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ shapesUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using shapes;
+
+namespace shapesUnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void area_with_no_inputs_should_be_0()
+        {
+            shape testShape = new shape();
+            testShape.display();
+
+            Assert.AreEqual(0, testShape.getArea());
+        }
+
+        [TestMethod]
+        public void perimeter_with_no_inputs_should_be_0()
+        {
+            shape testShape = new shape();
+            testShape.display();
+
+            Assert.AreEqual(0, testShape.getPerimeter());
+        }
+    }
+}
