5fc3dc05d8ca7e6d38d24bd6aa662bae765c4a3a
qthomson88@gmail.com <qthomson88@gmail.com>
2020-09-07 16:26:58 -0600

    add notes about further unit testing I would want to do

--- shapesUnitTestProject1/UnitTest1.cs
+++ shapesUnitTestProject1/UnitTest1.cs
@@ -24,5 +24,13 @@ namespace shapesUnitTestProject1
 
             Assert.AreEqual(0, testShape.getPerimeter());
         }
+
+        //ideally I would want to test the area and perimeter for both a triangle and rectangle with no inputs
+        //these tests would of course be in separate methods
+        //I would further want to test with inputs
+        //thinking through this, I realize, it would probably be good to make getters and setters for all
+        //the parameters so I could more easily test different inputs and outputs
+        //that's not to say I couldn't still have the display be the way I made it, I would just want to
+        //consider changing or adding some functions
     }
 }
